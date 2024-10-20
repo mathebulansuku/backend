c82ef3c959187f7bd605b1c2fa3f59040b2f8d9d
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-10-20 23:06:16 +0200

    Downloaded new npm modules

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// import generateName from "sillyname";
+import superheroes from "superheroes";
+
+// var sillyName = generateName();
+const name = superheroes;
+
+// console.log(`My name is ${sillyName}.`);
+console.log(`I am ${name}.`);
