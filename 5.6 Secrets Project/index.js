3270f96049e420dfd1d68a695fabf084c695e30f
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-02-07 16:32:33 +0200

    all dependencies installed and initialized

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -1,10 +1,19 @@
-// HINTS:
-// 1. Import express and axios
+import express from "express";
+import axios from "axios";
 
-// 2. Create an express app and set the port number.
+const app = express();
+const PORT = 3000;
 
-// 3. Use the public folder for static files.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(port, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "Waiting for data..." });
+});
 // 4. When the user goes to the home page it should render the index.ejs file.
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
