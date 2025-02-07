30a642dc28fa7dc10261911631c6670fcbc0c48a
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-02-07 16:46:11 +0200

    Added API to get file

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -1,18 +1,32 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
+const config = {
+  user: "Manny",
+};
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "Waiting for data..." });
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("URL", config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 // 4. When the user goes to the home page it should render the index.ejs file.
 
