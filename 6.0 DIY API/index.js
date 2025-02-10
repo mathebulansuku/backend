ff809b885c2e4bc843a649ed061004a6724590a5
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-02-10 13:48:32 +0200

    delete all jokes

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -90,6 +90,17 @@ app.delete("/delete", (req, res) => {
 });
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorized to perform this action.` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
